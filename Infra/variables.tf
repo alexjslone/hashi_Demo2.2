bec4aa28ed8a43ba97cc86a63894b6a9edba192f
Alex  Slone <alexjslone12@gmail.com>
2024-06-07 19:14:34 -0700

    updating with t2.large to test policy

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -20,5 +20,5 @@ variable "ami" {
 variable "instance_type" {
   description = "The type of EC2 instance used to create the instance."
   type        = string
-  default     = "t2.micro"
+  default     = "t2.large"
 }
